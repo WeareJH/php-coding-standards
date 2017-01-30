24f9bf1bad47122bbc3c9fe3b96229f6899aeab8
Aydin Hassan <aydin@hotmail.co.uk>
2017-01-30 19:31:57 +0100

    Initial commit

---
+++ Jh/Sniffs/Commenting/ClassCommentSniff.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * @author Aydin Hassan <aydin@wearejh.com>
+ */
+class Jh_Sniffs_Commenting_ClassCommentSniff implements PHP_CodeSniffer_Sniff
+{
+    private $requiredAttributes = [
+        '@author'
+    ];
+
+    public function register()
+    {
+        return [T_CLASS];
+    }
+
+    public function process(PHP_CodeSniffer_File $phpCsFile, $stackPtr)
+    {
+        $tokens = $phpCsFile->getTokens();
+        $find   = PHP_CodeSniffer_Tokens::$methodPrefixes;
+        $find[] = T_WHITESPACE;
+
+        $commentEnd = $phpCsFile->findPrevious($find, $stackPtr - 1, null, true);
+        if ($tokens[$commentEnd]['code'] !== T_DOC_COMMENT_CLOSE_TAG
+            && $tokens[$commentEnd]['code'] !== T_COMMENT
+        ) {
+            $phpCsFile->addError('Missing class doc comment', $stackPtr, 'Missing');
+            $phpCsFile->recordMetric($stackPtr, 'Class has doc comment', 'no');
+            return;
+        }
+
+        $phpCsFile->recordMetric($stackPtr, 'Class has doc comment', 'yes');
+
+        if ($tokens[$commentEnd]['code'] === T_COMMENT) {
+            $phpCsFile->addError('You must use "/**" style comments for a class comment', $stackPtr, 'WrongStyle');
+            return;
+        }
+
+        if ($tokens[$commentEnd]['line'] !== ($tokens[$stackPtr]['line'] - 1)) {
+            $error = 'There must be no blank lines after the class comment';
+            $phpCsFile->addError($error, $commentEnd, 'SpacingAfter');
+        }
+
+        $commentStart = $tokens[$commentEnd]['comment_opener'];
+
+        $tags = $tokens[$commentStart]['comment_tags'];
+        $tagLabels = [];
+        foreach ($tags as $tag) {
+            $tagLabels[$tag] = $tokens[$tag]['content'];
+        }
+
+        foreach ($tags as $tag) {
+            $tagLabel = $tagLabels[$tag];
+            if (!in_array($tagLabel, $this->requiredAttributes, true)) {
+                $error = '%s tag is not allowed in class comment';
+                $data  = array($tokens[$tag]['content']);
+                $phpCsFile->addError($error, $tag, 'TagNotAllowed', $data);
+            }
+        }
+
+        foreach ($this->requiredAttributes as $requiredAttribute) {
+            if (!in_array($requiredAttribute, $tagLabels, true)) {
+                $error = '%s tag must be in class comment';
+                $phpCsFile->addError($error, $commentStart, 'TagRequired', [$requiredAttribute]);
+                continue;
+            }
+
+            $pointer = array_search($requiredAttribute, $tagLabels, true);
+            $content = $tokens[$pointer + 2]['content'];
+
+            $validateMethod = 'validate' . ucfirst(substr($requiredAttribute, 1));
+            if (method_exists($this, $validateMethod) && !$this->$validateMethod($content)) {
+                $error = 'The value for %s tag is not valid: %s';
+                $phpCsFile->addError($error, $pointer + 2, 'TagNotValid', [$requiredAttribute, $content]);
+            }
+        }
+    }
+
+    private function validateAuthor($value)
+    {
+        $res = preg_match('/^(?P<name>[- .,\p{L}\p{N}\'’"()]+) <(?P<email>.+?)>$/u', $value, $matches);
+
+        if (!$res) {
+            return false;
+        }
+
+        return false !== filter_var($matches['email'], FILTER_VALIDATE_EMAIL);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # php-coding-standards
 Custom PHP CS Rules
+
+## Installation
+
+Add the following to your `composer.json` file:
+
+```
+"repositories" : [
+    {
+        "type": "vcs",
+        "url": "git@github.com:WeareJH/php-coding-standards.git"
+    }
+]
+```
+
+Then run:
+
+```
+composer require wearejh/php-coding-standards --dev
+```
+
+## Usage
+
+Assuming you have phpcs installed:
+
+```
+./vendor/bin/phpcs -s /folder/to/code --standard=vendor/wearejh/php-coding-standards/Jh
+```
